90129aa562a3c3d25f44e1a01b857c0a83b51fe9
Richard Krikler <rk@plan2.net>
2025-12-15 17:36:27 +0100

    [TASK] Allow TYPO3 v4

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,11 +8,11 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'office@plan2.net',
     'author_company' => 'plan2net GmbH',
     'state' => 'stable',
-    'version' => '13.1.0',
+    'version' => '14.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-13.4.99',
-            'php' => '8.1.0-8.3.99',
+            'typo3' => '12.4.0-14.4.99',
+            'php' => '8.1.0-8.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
